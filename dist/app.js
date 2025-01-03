23aa39bc4cd5e56646f2f4d31e68c7a08f3413cd
shivang-16 <shivangyadav121@gmail.com>
2025-01-03 13:19:46 +0530

    cors update

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,75 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
-const express_1 = __importDefault(require("express"));
+const express_1 = __importStar(require("express"));
 const dotenv_1 = require("dotenv");
 const issueRoutes_1 = __importDefault(require("./routes/issueRoutes"));
 const gsoc_1 = __importDefault(require("./routes/gsoc"));
 const data_1 = __importDefault(require("./routes/data"));
 const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 // Load environment variables
 (0, dotenv_1.config)({
     path: "./.env",
 });
 exports.app = (0, express_1.default)();
 // Middleware
+exports.app.use(((0, cookie_parser_1.default)()));
 exports.app.use(express_1.default.json());
-exports.app.use((0, cors_1.default)({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:3000'],
+exports.app.use((0, express_1.urlencoded)({ extended: true }));
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:3000"];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin)
+            return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1 ||
+            /\.vercel\.app$/.test(origin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+};
+exports.app.use((0, cors_1.default)(corsOptions));
 // Routes
 exports.app.get('/', async (req, res) => {
     res.send('Server is working!');
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express, { Application } from 'express';
+import express, { Application, urlencoded } from 'express';
 import {config} from 'dotenv';
 import issueRoutes from './routes/issueRoutes';
 import goscRoutes from './routes/gsoc';
 import dataRoutes from './routes/data';
 import cors from 'cors';
-import ConnectToDB from './db/db';
+import cookieParser from "cookie-parser";
 
 // Load environment variables
 config({
@@ -15,12 +15,30 @@ config({
 export const app: Application = express();
 
 // Middleware
+app.use((cookieParser()));
 app.use(express.json());
-app.use(cors({
-    origin:[process.env.FRONTEND_URL || 'http://localhost:3000'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(urlencoded({ extended: true }));
+
+const allowedOrigins = [process.env.FRONTEND_URL!, "http://localhost:3000"];
+
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+
+    if (
+      allowedOrigins.indexOf(origin) !== -1 ||
+      /\.vercel\.app$/.test(origin)
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // Routes
 app.get('/', async (req, res) => {
